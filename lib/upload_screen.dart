775b2054bdf69cd17f430ca7f7334ac66239a3d6
shanuji <123317565+shanuji@users.noreply.github.com>
2026-08-06 15:53:50 +0530

    Update upload_screen.dart

--- lib/upload_screen.dart
+++ lib/upload_screen.dart
@@ -55,7 +55,8 @@ class _UploadScreenState extends State<UploadScreen> {
     });
 
     try {
-      var uri = Uri.parse("https://fundwise-backend-coow.onrender.com/api/v1/parse-cas"); 
+      var uri = Uri.parse("https://fundwise-backend-coow.onrender.com/api/v1/parse-cas");
+ 
       
       var request = http.MultipartRequest('POST', uri);
       

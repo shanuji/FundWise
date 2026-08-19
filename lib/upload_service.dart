7c789de16c50ef89b6a1942bdcf4507f386e4b98
shanuji <123317565+shanuji@users.noreply.github.com>
2026-08-19 22:10:40 +0530

    Fix upload service network exception import

--- lib/upload_service.dart
+++ lib/upload_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:file_selector/file_selector.dart';
 import 'package:shared_preferences/shared_preferences.dart';
